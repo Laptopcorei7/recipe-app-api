1021b3f2a00496388e558561221af15523f93be1
laptopcorei7 <buabengasare56@gmail.com>
2024-06-21 20:30:14 +0000

    Added a create tag feature into recipe API.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,14 +14,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Entrypoint for command"""
-        self.stdout.write("Waitng fo database...")
+        self.stdout.write("Waitng for database...")
         db_up = False
         while db_up is False:
             try:
                 self.check(databases=["default"])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write("Database unavailable, waiting 1 second...")
+                self.stdout.write(
+                    "Database unavailable, waiting 1 second..."
+                )
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS("Database available!"))
