83f7d9aab147f2c531aebd35965d2e8f7b52e2c1
laptopcorei7 <buabengasare56@gmail.com>
2024-07-02 21:20:34 +0000

    Added functionality to include static files

--- app/app/settings.py
+++ app/app/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/static/"
-MEDIA_URL = "/static/media"
+MEDIA_URL = "/static/media/"
 
-MEDIA_URL = "/vol/web/media"
-STATIC_ROOT = "/vol/web/static"
+MEDIA_URL = "/vol/web/media/"
+STATIC_ROOT = "/vol/web/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
