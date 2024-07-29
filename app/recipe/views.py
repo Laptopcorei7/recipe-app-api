1f2de92f41a14e9da586fb253748f43103e96d0f
LaptopCoreI7 <buabengasare56@gmail.com>
2024-07-29 22:33:07 +0000

    Implemented the filter on tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,6 +4,12 @@ Views for recipe APIs.
 """
 
 from core.models import Ingredient, Recipe, Tag
+from drf_spectacular.utils import (
+    OpenApiParameter,
+    OpenApiTypes,
+    extend_schema,
+    extend_schema_view,
+)
 from recipe import serializers
 from rest_framework import mixins, status, viewsets
 from rest_framework.authentication import TokenAuthentication
@@ -20,10 +26,28 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        """Convert a list of list of strings to integers."""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
         """Retrieve recipes for authenticated users."""
-        return self.queryset.filter(user=self.request.user).order_by(
-            "-id"
+        tags = self.request.query_params.get("tags")
+        ingredients = self.request.query_params.get("ingredients")
+        queryset = self.queryset
+        if tags:
+            tags_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tags_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(
+                ingredients__id__in=ingredient_ids
+            )
+
+        return (
+            queryset.filter(user=self.request.user)
+            .order_by("id")
+            .distinct()
         )
 
     def get_serializer_class(self):
