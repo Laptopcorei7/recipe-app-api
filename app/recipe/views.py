e8d78e90b40e58e10659b1316fa124b230f98d2c
LaptopCoreI7 <buabengasare56@gmail.com>
2024-07-29 23:09:02 +0000

    Implemented the filter on tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -46,7 +46,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
         return (
             queryset.filter(user=self.request.user)
-            .order_by("id")
+            .order_by("-id")
             .distinct()
         )
 
